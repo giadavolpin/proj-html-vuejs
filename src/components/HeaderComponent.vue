e093b0618d2f721d45403eddbad8afafb96fd6ee
giadavolpin <volpingiada@gmail.com>
2022-11-21 11:23:41 +0100

    header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <div>
+            <img src="public\image\image\takeout-logo.png" alt="">
+        </div>
+        <div>
+            <button class="active">Home</button>
+            <button>Order Online</button>
+            <button>About</button>
+            <button>News</button>
+            <button>Contact Us</button>
+            <button><i class="fa-solid fa-cart-shopping"></i></button>
+        </div>
+
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
