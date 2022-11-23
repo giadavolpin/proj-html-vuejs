e29d577bc05e462dd806c724ea5d55aa6afe9387
giadavolpin <volpingiada@gmail.com>
2022-11-23 17:39:39 +0100

    footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,12 @@
 <template>
    <div class="imgback bianco">
-        <div class="d-flex ">
-            <ul class=" ">
+        <div class="d-flex justify-content-around">
+            <ul class="pt-2 ">
                 <li class="text-uppercase"><strong> order online </strong></li>
                  <li v-for="(item,index) in order" :key="index">{{item.name}}</li>
             </ul>
                
-            <ul class=" ">
+            <ul class="pt-2 ">
                 <li class="text-uppercase"><strong> navigate</strong></li>
                 <li>Home</li>
                 <li>Alternate Home</li>
@@ -17,22 +17,22 @@
                 <li>Cart</li>
             </ul>
       
-            <div class="">    
+            <div class="pt-2">    
                 <ul class=" ">
                     <li class="text-uppercase"><strong> information</strong></li>
                     <li>My Account</li>
                     <li>Terms Of Service</li>
                     <li>Privacy Policy</li>
                 </ul>
-                <div class="">
-                    <img class="" src="/public/image/app-store-badge.png" alt="">
+                <div class="pt-2">
+                    <img class="bordo" src="/public/image/app-store-badge.png" alt="">
                 </div>
                 <div>    
-                    <img class="" src="/public/image/play-store-badge.png" alt="">
+                    <img class="bordo" src="/public/image/play-store-badge.png" alt="">
                 </div>
             </div>
 
-            <ul>
+            <ul  class="pt-2">
                 <li class="text-uppercase"><strong> opening hours </strong></li>
                 <li>Mon - <span class="giallo">10AM to 11PM</span></li>
                 <li>Tue- <span class="giallo">10AM to 11PM</span></li>
@@ -42,8 +42,32 @@
                 <li>Sat - <span class="giallo">10AM to 11PM</span></li>
                 <li>Sun - <span class="giallo">10AM to 11PM</span></li>
             </ul>
-        </div>    
-   </div>
+            <div  class="pt-2">
+                <ul>
+                    <li class="text-uppercase"><strong> voucher</strong></li>
+                    <img class="sconto" src="/public/image/footer-coupon.png" alt="">
+                    <li class="bianco ext"> <strong> Just Use The Code</strong></li>
+                    <li class="text-uppercase ext giallo"><strong> firstorder</strong></li>
+                    <li class="bianco ext"><strong> AtCheckout</strong></li>
+                </ul>
+            </div>
+        </div>  
+        
+        <div class="">
+            <div class="allgv">
+                <img  src="/public/image/takeout-logo.png" alt="">
+            </div>
+            <h2 class="allgv">Stay in The Loop</h2>
+            <p class="allgv">Sign up to receive up to date news and offers directly in your inbox:</p>
+            <div class="allgv">
+                <input class="radius" type="text" placeholder="you@email.com">
+            </div>
+            <div class="allgv">
+                <button type="button" class="btn btn-danger radius text-uppercase">subscribe</button>
+            </div>
+        </div>
+
+   </div> 
 </template>
 
 <script>
@@ -100,6 +124,20 @@ export default {
     background-size: cover;
     background-repeat: no-repeat;
 }
+.radius{
+    border-radius: 20px;
+    width: 500px;
+}
+.allgv{
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+
+}
+.ext{
+    font-size: 12px;
+    text-align: center;
+}
 .giallo{
     color: $giallo;
 }
@@ -114,8 +152,16 @@ li{
 li:hover{
     color: $giallo;
 }
-img{
+.bordo{
     width: 150px;
     cursor: pointer;
+    border: 2px solid $white;
+    border-radius: 10px;
+    margin-bottom: 10px;
 }
+.sconto{
+    width: 100px;
+    height: 100px;
+}
+
 </style>
